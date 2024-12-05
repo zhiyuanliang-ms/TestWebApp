3ae3961bffa2f2117cee1e9f8e4424567188808c
zhiyuanliang <zhiyuanliang@microsoft.com>
2024-12-06 03:11:18 +0800

    update

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 import http from 'http';
+import { load } from '@azure/app-configuration-provider'
+
+
+const settings = await load("Endpoint=https://zhiyuanliang-ac.azconfig.io;Id=Zr7u;Secret=EBgweCLsAx3jofS5pIWsmV3QMK7ZwwYHAGISGcIP6EIn8224gWkmJQQJ99AKACkVi7rAArohAAACAZAC3LJ3")
+
+const test = settings.get("test");
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
 
   res.end(`
-    <h1>Hello World!</h1>
+    <h1>Hello World! ${test}</h1>
     <p>URL: ${req.url}</p>
     <p>Method: ${req.method}</p>
     <p>Headers:</p>
